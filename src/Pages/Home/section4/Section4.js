08f870fe85381209899417c8408e748fb08c7bb2
LokeshSingh09 <lokeshrajput0009@gmail.com>
2023-02-18 01:10:57 +0530

    first'

---
+++ src/Pages/Home/section4/Section4.js
@@ -0,0 +1,230 @@
+import React from 'react'
+import './section4.css'
+
+const Section4 = () => {
+  return (
+    <div className='section4'>
+
+      <div className='comunity-box'>
+        <h1 className='section4_head'>Our Community</h1>
+      </div>
+      <div className='comunity-member-box'>
+
+        <div className='member-div-row1'>
+          <div className='row1-image-data'>
+            <div className='row1-img'>
+              <img src="../../community/Lokesh.jpg" alt="" />
+            </div>
+            <div className='row1-member-name'>
+              <h3>Lokesh</h3>
+            </div>
+          </div>
+
+          <div className='row1-image-data'>
+            <div className='row1-img'>
+              <img src="../../community/Chaman.jpg" alt="" />
+            </div>
+            <div className='row1-member-name'>
+              <h6>Chaman </h6>
+            </div>
+          </div>
+
+          <div className='row1-image-data'>
+            <div className='row1-img'>
+              <img src="../../community/Himanshu.jpg" alt="" />
+            </div>
+            <div className='row1-member-name'>
+              <h6>Himanshu </h6>
+            </div>
+          </div>
+
+          <div className='row1-image-data'>
+            <div className='row1-img'>
+              <img src="../../community/Gaurav.jpg" alt="" />
+            </div>
+            <div className='row1-member-name'>
+              <h6>Gaurav</h6>
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className='member-div-row2'>
+          <div className='row2-image-data'>
+            <div className='row2-img'>
+              <img src="../../community/Vaibhav.jpg" alt="" />
+            </div>
+            <div className='row2-member-name'>
+              <h6>Vaibhav </h6>
+            </div>
+          </div>
+
+          <div className='row2-image-data'>
+            <div className='row2-img'>
+              <img src="../../community/Harsh.jpg" alt="" />
+            </div>
+            <div className='row2-member-name'>
+              <h6>Harsh </h6>
+            </div>
+          </div>
+
+          <div className='row2-image-data'>
+            <div className='row2-img'>
+              <img src="../../community/Priyanshu.jpg" alt="" />
+            </div>
+            <div className='row2-member-name'>
+            <h6>Priyanshu </h6>
+            </div>
+          </div>
+
+          <div className='row2-image-data'>
+            <div className='row2-img'>
+              <img src="../../community/Anshuman.jpg" alt="" />
+            </div>
+            <div className='row2-member-name'>
+              <h6>Anshuman </h6>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        {/* <div className='member-div-row3'>
+          <div className='row3-image-data'>
+            <div className='row3-img'>
+              <img src="../../co1.jpg" alt="" />
+            </div>
+            <div className='row3-member-name'>
+              <h6>Lokesh </h6>
+            </div>
+          </div>
+
+          <div className='row3-image-data'>
+            <div className='row3-img'>
+              <img src="../../co1.jpg" alt="" />
+            </div>
+            <div className='row3-member-name'>
+              <h6>Lokesh </h6>
+            </div>
+          </div>
+
+          <div className='row3-image-data'>
+            <div className='row3-img'>
+              <img src="../../co1.jpg" alt="" />
+            </div>
+            <div className='row3-member-name'>
+              <h6>Lokesh </h6>
+            </div>
+          </div>
+
+          <div className='row3-image-data'>
+            <div className='row3-img'>
+              <img src="../../co1.jpg" alt="" />
+            </div>
+            <div className='row3-member-name'>
+              <h6>Lokesh </h6>
+            </div>
+          </div>
+        </div> */}
+
+
+
+
+        <div className='member-div-row4'>
+          <div className='row4-image-data'>
+            <div className='row4-img'>
+              <img src="../../community/Chetan.jpg" alt="" />
+            </div>
+            <div className='row4-member-name'>
+              <h6>Chetan </h6>
+            </div>
+          </div>
+
+          <div className='row4-image-data'>
+            <div className='row4-img'>
+              <img src="../../community/Riyansh.jpg" alt="" />
+            </div>
+            <div className='row4-member-name'>
+              <h6>Riyansh </h6>
+            </div>
+          </div>
+
+          <div className='row4-image-data'>
+            <div className='row4-img'>
+              <img src="../../community/Vikas.jpg" alt="" />
+            </div>
+            <div className='row4-member-name'>
+              <h6>Vikas </h6>
+            </div>
+          </div>
+
+          <div className='row4-image-data'>
+            <div className='row4-img'>
+              <img src="../../community/Sachin.jpg" alt="" />
+            </div>
+            <div className='row4-member-name'>
+              <h6>Sachin </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <ul className='section_img'>
+        <li>
+          <img src="../../co1.jpg" alt="" />
+          <h6>Lokesh </h6>
+        </li>
+        <li>
+          <img src="../../co2.jpg" alt="" />
+          <h6>Chaman </h6>
+        </li>
+        <li>
+          <img src="../../co3.jpg" alt="" />
+          <h6>Anshuman</h6>
+        </li>
+        <li>
+          <img src="../../co1.jpg" alt="" />
+          <h6>Gaurav </h6>
+        </li>
+        <li>
+          <img src="../../co2.jpg" alt="" />
+          <h6>Himanshu </h6>
+        </li>
+        <li>
+          <img src="../../co3.jpg" alt="" />
+          <h6>Akash  </h6>
+        </li>
+        <li>
+          <img src="../../co3.jpg" alt="" />
+          <h6>Vikas  </h6>
+        </li>
+        <li>
+          <img src="../../co3.jpg" alt="" />
+          <h6>Priyanshu </h6>
+        </li>
+        <li>
+          <img src="../../co3.jpg" alt="" />
+          <h6>Chetan </h6>
+        </li>
+      </ul> */}
+
+    </div>
+  )
+}
+
+export default Section4
